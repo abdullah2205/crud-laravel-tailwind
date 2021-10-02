ce462edca9743345356945a0ac4393f1f2b42549
kuuhaku <ma22052000@gmail.com>
2021-10-02 15:42:23 +0700

    install tailwind css

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-gray-200 px-4 py-2 text-black">
+		halo
+	</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //$data['anime'] = Anime::all();
+    return view('app'); //, $data);
 });
+
+Route::resource('anime', 'AppController');
